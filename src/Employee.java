4b5eee60669f90de7b02db444ba4ba7357a66e24
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2021-02-13 15:37:55 -0500

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -4,12 +4,36 @@
  * @author David Ha, Andrew McAvoy
  */
 public class Employee {
+    private Profile profile;
+    private double payment;
 
     /**
      * Constructor for employee class
      */
-    public Employee(){
+    public Employee(Profile profile){
+        this.profile = profile;
+    }
+
+    /**
+     * Prints instance of this class in the following format
+     * profile::payment
+     *
+     * @return textual representation of this class
+     */
+    
+    @Override
+    public String toString(){
 
     }
 
+    /**
+     * Checks if two employees are equal
+     *
+     * @param obj object to be compared
+     * @return true if equal; false if otherwise
+     */
+    @Override
+    public boolean equals(Object obj){
+
+    }
 }
