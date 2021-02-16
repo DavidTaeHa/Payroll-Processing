990b253b6c9a462f97d2b91a15e5701bd066f2a6
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2021-02-16 16:43:36 -0500

    Update PayrollProcessing.java

--- src/PayrollProcessing.java
+++ src/PayrollProcessing.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 /**
  * This class will handle all user input and output
  *
  * @author David Ha, Andrew McAvoy
  */
 public class PayrollProcessing {
+    private boolean payrollProcessingSession;
 
+    /**
+     * Constructor for PayrollProcessing class
+     */
+    public PayrollProcessing(){
+        payrollProcessingSession = true;
+    }
+
+    /**
+     * Handles all user input and output
+     */
+    public void run(){
+        Company company = new Company();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Payroll Processing Session Running.");
+    }
 }
