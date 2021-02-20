528ec709a0a8218a70e5fe7c3cc59f4b273c9301
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2021-02-20 17:30:10 -0500

    Add JUnit

---
+++ src/ManagementTest.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ManagementTest {
+
+    @org.junit.jupiter.api.Test
+    void calculatePayment() {
+        
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+
+    }
+}
